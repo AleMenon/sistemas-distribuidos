650e05f841124d0828cdd5f873d2d41cefce9a10
Alexandre Alberto Menon <alexandrealberto.menon@gmail.com>
2026-09-13 17:37:08 -0300

    RabbitMQ: Assinatura implementada no estoque

--- rabbitMQ/estoque/estoque.py
+++ rabbitMQ/estoque/estoque.py
@@ -1,11 +1,18 @@
 import pika
 import random
 import json
+import base64
+
 from pika.adapters.blocking_connection import BlockingChannel
 from pika.spec import Basic, BasicProperties
+from pika.spec import Basic, BasicProperties
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 
 EXCHANGE = 'ecommerce'
 QUEUE = 'fila.estoque'
+KEY_NAME = 'estoque'
 
 PRODUCTS = [
     {
@@ -35,31 +42,57 @@ channel.queue_declare(queue=QUEUE, exclusive=True)
 for v in ['criado', 'excluido']:
     channel.queue_bind(exchange=EXCHANGE, queue=QUEUE, routing_key=f'pedido.{v}')
 
-def callback(ch: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: bytes):
-    if method.routing_key == 'pedido.criado':
-        order = json.loads(bytes.decode(body))
+def sign_content(body):
+    event_str = json.dumps(body['content'], sort_keys=True)
+    event_hash = SHA256.new(event_str.encode('utf-8'))
 
-        for item in order['items']:
+    with open(f'{KEY_NAME}_private.pem', 'r') as f:
+        priv_key = RSA.import_key(f.read())
+
+    signature = pkcs1_15.new(priv_key).sign(event_hash)
+    body['signature'] = base64.b64encode(signature).decode('utf-8')
+
+    return json.dumps(body)
+
+def validate_signature(body, src):
+    event_str = json.dumps(body['content'], sort_keys=True)
+    event_hash = SHA256.new(event_str.encode('utf-8'))
+
+    try:
+        with open(f'{src}_public.pem', 'r') as f:
+            pub_key = RSA.import_key(f.read())
+        pkcs1_15.new(pub_key).verify(event_hash, body['signature'])
+    except ValueError:
+        print("ASSINATURA INVÁLIDA! Evento adulterado ou de fonte desconhecida. Descartando...")
+        return False
+    return True
+
+def callback(ch: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: bytes):
+    json_body = json.loads(bytes.decode(body))
+
+    if not validate_signature(json_body, 'main'):
+        ch.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
+        return
+
+    if method.routing_key == 'pedido.criado':
+
+        for item in json_body['content']['items']:
             target = next(
                 product for product in PRODUCTS
                 if product['id'] == item['id']
             )
 
             if item['quantity'] > target['stock']:
-                response = {
-                    'order_id': order['id'],
-                    'product_id': target['id']
-                }
-                ch.basic_publish(exchange=EXCHANGE, routing_key='estoque.indisponivel', body=json.dumps(response))
+                str_body = sign_content(json_body)
+                ch.basic_publish(exchange=EXCHANGE, routing_key='estoque.indisponivel', body=str_body)
                 return
 
             target['stock'] -= item['quantity']
-            ch.basic_publish(exchange=EXCHANGE, routing_key='pedido.estoque_ok', body=str(order['id']))
+            str_body = sign_content(json_body)
+            ch.basic_publish(exchange=EXCHANGE, routing_key='pedido.estoque_ok', body=str_body)
 
     elif method.routing_key == 'pedido.excluido':
-        order = json.loads(bytes.decode(body))
-
-        for item in order['items']:
+        for item in json_body['content']['items']:
             target = next(
                 product for product in PRODUCTS
                 if product['id'] == item['id']
