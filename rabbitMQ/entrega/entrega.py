e90b984524dc78966e3bc5a607824b10411af7d9
Alexandre Alberto Menon <alexandrealberto.menon@gmail.com>
2026-09-13 17:48:58 -0300

    RabbitMQ: Assinatura implementada na entrega

--- rabbitMQ/entrega/entrega.py
+++ rabbitMQ/entrega/entrega.py
@@ -1,9 +1,16 @@
 import pika
+import json
+import base64
+
 from pika.adapters.blocking_connection import BlockingChannel
 from pika.spec import Basic, BasicProperties
+from Crypto.PublicKey import RSA
+from Crypto.Signature import pkcs1_15
+from Crypto.Hash import SHA256
 
 EXCHANGE = 'ecommerce'
 QUEUE = 'fila.entrega'
+KEY_NAME = 'entrega'
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
@@ -14,8 +21,40 @@ channel.queue_declare(queue=QUEUE, exclusive=True)
 
 channel.queue_bind(exchange=EXCHANGE, queue=QUEUE, routing_key='pagamento.aprovado')
 
+def sign_content(body):
+    event_str = json.dumps(body['content'], sort_keys=True)
+    event_hash = SHA256.new(event_str.encode('utf-8'))
+
+    with open(f'{KEY_NAME}_private.pem', 'r') as f:
+        priv_key = RSA.import_key(f.read())
+
+    signature = pkcs1_15.new(priv_key).sign(event_hash)
+    body['signature'] = base64.b64encode(signature).decode('utf-8')
+
+    return json.dumps(body)
+
+def validate_signature(body, src):
+    event_str = json.dumps(body['content'], sort_keys=True)
+    event_hash = SHA256.new(event_str.encode('utf-8'))
+
+    try:
+        with open(f'{src}_public.pem', 'r') as f:
+            pub_key = RSA.import_key(f.read())
+        pkcs1_15.new(pub_key).verify(event_hash, body['signature'])
+    except ValueError:
+        print("ASSINATURA INVÁLIDA! Evento adulterado ou de fonte desconhecida. Descartando...")
+        return False
+    return True
+
 def callback(ch: BlockingChannel, method: Basic.Deliver, properties: BasicProperties, body: bytes):
+    json_body = json.loads(bytes.decode(body))
+
+    if not validate_signature(json_body, 'pagamento'):
+        ch.basic_reject(delivery_tag=method.delivery_tag, requeue=False)
+        return
+
     if method.routing_key == 'pagamento.aprovado':
+        str_body = sign_content(json_body)
         ch.basic_publish(exchange=EXCHANGE, routing_key='pedido.enviado', body=body)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
